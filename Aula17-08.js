4f857a02c803318587a9e9b121077ab87836b17a
OrquideaFlorida <140519321+OrquideaFlorida@users.noreply.github.com>
2023-08-22 14:11:00 +0000

    Aula17-08.js

--- Aula17-08.js
+++ Aula17-08.js
@@ -71,17 +71,82 @@
 
 //EXERCÍCIO 1
 
-try {
-    console.log("Calculos");
-    let input = 0;
-    if (input === 0) {
+// try {
+//     console.log("Calculos");
+//     let input = 0;
+//     if (input === 0) {
 
-        throw "Valor negado";
+//         throw "Valor negado";
+//     }
+//     let result = 15 / input
+// } catch (erro) {
+//     console.log("[Erro] Divisão por zero não permitida");
+// }finally {
+//     console.log("Finalizando");
+// }
+
+
+//----------------------------------------------------------
+
+//22/08
+
+//EXERCICIO 1
+
+// const numeros = [3, 7, 2, 9, 5]
+  
+// // for (let indice in numeros){   //Usando for in
+// //     console.log(numeros[indice]);
+// //     }
+
+// for (let indice of numeros){  //Usando For Of
+// //     console.log(indice)
+// }
+
+//EXERCICIO 2
+
+// const lista = {A: 10, B: 20, C: 30, D: 40}
+// let soma = 0
+// for (let index in lista){
+//     soma += lista[index]
+// }
+//     console.log(soma)
+
+//-------------------------------------------------------
+
+//EXERCICIO 3
+
+// const lista = [12, 5, 8, 21, 16, 7]
+// const resultado = []
+// for (let par of lista){
+//     if (par % 2 == 0){
+//         resultado.push(par)
+//     }
+// }
+// console.log(resultado)
+
+//EXERCICIO 4
+
+// const estudantes = { alice: 18, bob: 20, carol: 19, david: 21 };
+
+// for (let index in estudantes){
+//     if (estudantes[index] == 19){
+//         console.log(`Nome: ${index} Idade: ${estudantes[index]}`)
+//     }
+// }
+
+//EXERCICIO 5
+
+const pontos = [ [2, 5, 8], [3, 9, 12], [4, 6, 10] ]
+let soma = 0
+for (let linha in pontos){
+    for (let coluna in pontos[linha]){
+        soma += pontos[linha][coluna]
+        // console.log(pontos[1][1])
     }
-    let result = 15 / input
-} catch (erro) {
-    console.log("[Erro] Divisão por zero não permitida");
-}finally {
-    console.log("Finalizando");
 }
+
+//Quando tiver um vetor dentro de outro vetor, indicar a posição da linha e da coluna.
+
+
+
 
