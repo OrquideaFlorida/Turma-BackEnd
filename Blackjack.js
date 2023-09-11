ae303f9e54d61fa536b496d7ae1eda32d65a9688
OrquideaFlorida <140519321+OrquideaFlorida@users.noreply.github.com>
2023-09-11 00:15:22 +0000

    Ajustando o jogo

---
+++ Blackjack.js
@@ -0,0 +1,48 @@
+//DESAFIO 1: JOGADOR
+const readline = require('readline-sync');
+
+let carta1 = readline.questionInt('Escolha a primeira carta de 1 a 10: ');
+let carta2 = readline.questionInt('Escolha a segunda carta de 1 a 10: ');
+
+let soma = carta1 + carta2
+
+if (soma > 21) {
+    console.log(`Total de pontos: ${soma}. Estourou! Você Perdeu.`);
+}else if (soma === 21){
+    console.log(`Você fez ${soma}! Você ganhou!!!`);
+}else {
+    console.log(`Você fez ${soma} pontos. Abaixo de 21. Deseja continuar? `);
+    let decid = readline.question('Escolha [S] ou [N]: ');
+        switch(decid){
+            case 'S':
+                //FAZER COM QUE SE VOLTE PARA ADICIONAR MAIS CARTAS
+                break;
+            case 'N':
+                //console.log(`Com ${soma} pontos adquiridos, você pode vencer!`)
+                //loop = false
+                break;
+            default:
+                console.log("Escolha apenas S ou N");
+                break;
+        }
+}
+
+//DESAFIO 2: COM A BANCA ADVERSÁRIA
+
+//let carta1 = 10
+// let carta2 = 11
+// let soma = carta1 + carta2
+
+// let cartab1 = 2
+// let cartab2 = 2
+// let cartab3 = 2
+// let cartab4 = 2
+// let somaBanco = cartab1 + cartab2 + cartab3 + cartab4
+
+// if (somaBanco > 21) {
+//     console.log('O Banco Perdeu');
+// }else if (soma>somaBanco && soma <= 21){
+//     console.log("Você ganhou para o banco");
+// }else {
+//     console.log ("O Banco ganhou");
+// }
